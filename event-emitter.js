2c2bf9d38533b09c6b0422e39eeccc85c882623f
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2025-04-20 09:58:44 +0300

    add `on` and `emit` methods

--- event-emitter.js
+++ event-emitter.js
@@ -1,3 +1,40 @@
-class MyEventEmitter {}
+class MyEventEmitter {
+  /**@type {Map<string, Array<Function>} */
+  #eventListeners;
+  /**@type {Map<string, Array<Function>} */
+  #eventListenersOnce;
+
+  constructor() {
+    this.#eventListeners = new Map();
+    this.#eventListenersOnce = new Map();
+  }
+
+  on(event, listener) {
+    this.#validateListener(listener);
+
+    const listeners = this.#eventListeners.get(String(event));
+
+    if (!listeners) {
+      this.#eventListeners.set(String(event), [listener]);
+      return;
+    }
+
+    this.#eventListeners.set(String(event), [...listeners, listener]);
+  }
+
+  emit(event, ...values) {
+    const listeners = this.#eventListeners.get(String(event));
+
+    if (!listeners) return;
+
+    listeners.forEach((listener) => listener(...values));
+  }
+
+  #validateListener(cb) {
+    if (typeof cb !== "function") {
+      throw new Error(`Listener should be a function, received ${cb}`);
+    }
+  }
+}
 
 module.exports = MyEventEmitter;
